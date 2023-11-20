63aed259512215a4438576e676f76c3399b7b2eb
umaparvathidevi <82662731+umaparvathidevi@users.noreply.github.com>
2023-11-20 11:08:12 +0530

    Add files via upload

---
+++ QuoteOfTheDay.java
@@ -0,0 +1,415 @@
+import java.util.Random;
+import javax.swing.JOptionPane;
+public class QuoteOfTheDay 
+{
+	public static void main(String[] args) 
+        {
+		Random random=new Random();
+		int a = random.nextInt(100);
+		JOptionPane.showMessageDialog(null,"Quote of the day!");
+		if(a==0) 
+		{ 
+			JOptionPane.showMessageDialog(null,"You are enough just as you are.");
+		}
+		else if(a==1) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Make each day your masterpiece.");
+		}
+		else if(a==2) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Try to be a rainbow in someone\'s cloud.");
+		}
+		else if(a==3) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Whatever you are, be a good one.");
+		}
+		else if(a==4) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Only when we are drowning do we understand how fierce our feet can kick.");
+		}
+		else if(a==5) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Life is like riding a bicycle. To keep your balance, you must keep moving.");
+		}
+		else if(a==6) 
+		{ 
+			JOptionPane.showMessageDialog(null,"You must be the best judge of your own happiness.");
+		}
+		else if(a==7) 
+		{ 
+			JOptionPane.showMessageDialog(null,"You must be the change you wish to see in the world.");
+		}
+		else if(a==8) 
+		{ 
+			JOptionPane.showMessageDialog(null,"No pressure, no diamonds.");
+		}
+		else if(a==9) 
+		{ 
+			JOptionPane.showMessageDialog(null,"There are two ways of spreading light  to be the candle or the mirror that reflects it.");
+		}
+		else if(a==10) 
+		{ 
+			JOptionPane.showMessageDialog(null,"You do not find the happy life. You make it.");
+		}
+		else if(a==11) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Believe you can and you are halfway there.");
+		}
+		else if(a==12) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Failure is the condiment that gives success its flavor.");
+		}
+		else if(a==13) 
+		{ 
+			JOptionPane.showMessageDialog(null,"It is never too late to be what you might have been.");
+		}
+		else if(a==14) 
+		{ 
+			JOptionPane.showMessageDialog(null,"When you have a dream, you have got to grab it and never let go.");
+		}
+		else if(a==15) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Life changes very quickly, in a very positive way, if you let it.");
+		}
+		else if(a==16) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Stay foolish to stay sane.");
+		}
+		else if(a==17) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Stay hungry. Stay foolish.");
+		}
+		else if(a==18) 
+		{ 
+			JOptionPane.showMessageDialog(null,"You must do the things you think you cannot do.");
+		}
+		else if(a==19) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Wherever you go, go with all your heart.");
+		}
+		else if(a==20) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Be faithful to that which exists within yourself.");
+		}
+		else if(a==21) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Dream big and dare to fail.");
+		}
+		else if(a==22) 
+		{ 
+			JOptionPane.showMessageDialog(null,"My mission in life is not merely to survive, but to thrive.");
+		}
+		else if(a==23) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Happiness is not by chance, but by choice.");
+		}
+		else if(a==24) 
+		{ 
+			JOptionPane.showMessageDialog(null,"To be the best, you must be able to handle the worst.");
+		}
+		else if(a==25) 
+		{ 
+			JOptionPane.showMessageDialog(null,"No matter what you are going through, there as a light at the end of the tunnel.");
+		}
+		else if(a==26) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Happiness often sneaks in through a door you didn\'t know you left open.");
+		}
+		else if(a==27) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Every moment is a fresh beginning.");
+		}
+		else if(a==28) 
+		{ 
+			JOptionPane.showMessageDialog(null,"No guts, no story.");
+		}
+		else if(a==29) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Everybody stay positive no matter how negative life gets.");
+		}
+		else if(a==30) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Your mind must be stronger than your feelings.");
+		}
+		else if(a==31) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Gotta keep my head above water, gotta make it through.");
+		}
+		else if(a==32) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Never break or fold, that is what it takes to be major.");
+		}
+		else if(a==33) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I know that now, and now is all that matters.");
+		}
+		else if(a==34) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Life is all about the evolution");
+		}
+		else if(a==35) 
+		{ 
+			JOptionPane.showMessageDialog(null,"No matter where life takes me, find me with a smile.");
+		}
+		else if(a==36) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I am the smartest man alive!");
+		}
+		else if(a==37) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Once you really know yourself, can not nobody tell you nothing about you.");
+		}
+		else if(a==38) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Sometimes you have to take two steps back to take ten forward.");
+		}
+		else if(a==39) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Our future is our confidence and self-esteem.");
+		}
+		else if(a==40) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Do not be scared to live your life without judgment");
+		}
+		else if(a==41) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Be kind to one another, even when it is not requested.");
+		}
+
+		else if(a==42) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I am my inspiration.");
+		}
+
+		else if(a==43) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Look at the sky tonight. All the stars have a reason.");
+		}
+		else if(a==44) 
+		{ 
+			JOptionPane.showMessageDialog(null,"What a dog I got. His favorite bone is in my arm.");
+		}
+		else if(a==45) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Only thing free in life is options.");
+		}
+		else if(a==46) 
+		{ 
+			JOptionPane.showMessageDialog(null,"There is no way to happiness, happiness is the way.");
+		}
+		else if(a==47) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I am not afraid of dying, I am afraid of not trying.");
+		}
+		else if(a==48) 
+		{ 
+			JOptionPane.showMessageDialog(null,"To lose your path is the way to find that path.");
+		}
+		else if(a==49) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I will not let anyone scare me out of my full potential.");
+		}
+		else if(a==50) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Peace is the absence of confusion.");
+		}
+		else if(a==51) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Our weary eyes still stray to the horizon.");
+		}
+		else if(a==52) 
+		{ 
+			JOptionPane.showMessageDialog(null,"In every walk with nature one receives far more than he seeks.");
+		}
+		else if(a==53) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I speak my own sins; I cannot judge another. I have no tongue for it.");
+		}
+		else if(a==54) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I work hard. But I play hard, too.");
+		}
+		else if(a==55) 
+		{ 
+			JOptionPane.showMessageDialog(null,"It is never too late to get good at something.");
+		}
+		else if(a==56) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Wear your crown and go chase your dreams.");
+		}
+		else if(a==57) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Life is what you make it, just do not fake it.");
+		}
+		else if(a==58) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Muddy water is best cleared by leaving it alone.");
+		}
+		else if(a==59) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Education and work are the levers to uplift a people.");
+		}
+		else if(a==60) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Do not wait for them to tell you. Tell them.");
+		}
+		else if(a==61) 
+		{ 
+			JOptionPane.showMessageDialog(null,"There can only be one king.");
+		}
+		else if(a==62) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I paint flowers so they will not die.");
+		}
+		else if(a==63) 
+		{ 
+			JOptionPane.showMessageDialog(null,"If I have a platform and a voice, I should use it for my people.");
+		}
+		else if(a==64) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Your river is strong. Let it flow.");
+		}
+		else if(a==65) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Knock me down 9 times, but I get up 10.");
+		}
+		else if(a==66) 
+		{ 
+			JOptionPane.showMessageDialog(null,"The beginning is the most important part of the work.");
+		}
+		else if(a==67) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Nothing left to do but smile.");
+		}
+		else if(a==68) 
+		{ 
+			JOptionPane.showMessageDialog(null,"The best revenge is massive success.");
+		}
+		else if(a==69) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Never to suffer would never to have been blessed.");
+		}
+		else if(a==70) 
+		{ 
+			JOptionPane.showMessageDialog(null,"The harder I work, the luckier I become.");
+		}
+		else if(a==71) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Luck is not a business model.");
+		}
+		else if(a==72) 
+		{ 
+			JOptionPane.showMessageDialog(null,"If there is no struggle, there is no progress.");
+		}
+		else if(a==73) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Behind every successful person lies a pack of haters");
+		}
+		else if(a==74) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Time is a wonderful storyteller.");
+		}
+		else if(a==75) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Yesterday do not matter if it is gone.");
+		}
+		else if(a==76) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I am like any other man. All I do is supply a demand.");
+		}
+		else if(a==77) 
+		{ 
+			JOptionPane.showMessageDialog(null,"An intelligent woman does not get sad, she gets pretty.");
+		}
+		else if(a==78) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Do not be afraid to give up the good to go for the great.");
+		}
+		else if(a==79) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I do not want to end up simply having visited this world.");
+		}
+		else if(a==80) 
+		{ 
+			JOptionPane.showMessageDialog(null,"The choices we make dictate the lives we lead.");
+		}
+		else if(a==81) 
+		{ 
+			JOptionPane.showMessageDialog(null,"We have added years to life, not life to years.");
+		}
+		else if(a==82) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Fight with a happy heart and a strong spirit.");
+		}
+		else if(a==83) 
+		{ 
+			JOptionPane.showMessageDialog(null,"It does not hurt to get more education.");
+		}
+		else if(a==84) 
+		{ 
+			JOptionPane.showMessageDialog(null,"We can be heroes just for one day.");
+		}
+		else if(a==85) 
+		{ 
+			JOptionPane.showMessageDialog(null,"You do not get respect if you do not deserve it.");
+		}
+		else if(a==86) 
+		{ 
+			JOptionPane.showMessageDialog(null,"You must work in the dark for your light to shine.");
+		}
+		else if(a==87) 
+		{ 
+			JOptionPane.showMessageDialog(null,"I do not need a lot, I just want to find my peace.");
+		}
+		else if(a==88) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Tomorrow hopes we have learned something from yesterday.");
+		}
+		else if(a==89) 
+		{ 
+			JOptionPane.showMessageDialog(null,"If you learned a lesson it was not a loss.");
+		}
+		else if(a==90) 
+		{ 
+			JOptionPane.showMessageDialog(null,"All humor is rooted in pain.");
+		}
+		else if(a==91) 
+		{ 
+			JOptionPane.showMessageDialog(null,"Do not cry because it is over. smile because it happened.");
+		}
+		else if(a==92)
+		{ 
+			JOptionPane.showMessageDialog(null,"You are not the opinion of someone who does not know you.");
+		}
+		else if(a==93)
+		{ 
+			JOptionPane.showMessageDialog(null,"Education is not preparation for life; education is life itself.");
+		}
+		else if(a==94)
+		{ 
+			JOptionPane.showMessageDialog(null,"Sometimes the heart is deceiving.");
+		}
+		else if(a==95)
+		{ 
+			JOptionPane.showMessageDialog(null,"Until death, it is all life.");
+		}
+		else if(a==96)
+		{ 
+			JOptionPane.showMessageDialog(null,"Being realistic is the quickest path to mediocrity.");
+		}
+		else if(a==97)
+		{ 
+			JOptionPane.showMessageDialog(null,"Governments change… the lies stay the same.");
+		}
+		else if(a==98)
+		{ 
+			JOptionPane.showMessageDialog(null,"Never confuse a single defeat with a final defeat.");
+		}
+		else if(a==99)
+		{ 
+			JOptionPane.showMessageDialog(null,"Good things come in good time.");
+		}
+
+	}
+
+}
